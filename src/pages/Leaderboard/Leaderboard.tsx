425cb1d93ff82ea2ef3b758523a5830dfa1b4ac1
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2024-01-12 15:25:19 +0700

    Update Leaderboard.tsx

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -13,24 +13,6 @@ export default function LeaderboardPage() {
       className="leaderboard flex w-[1920px] h-[1080px] flex-col "
       style={{ backdropFilter: "blur(48px)" }}
     >
-      <div className="h-[80px] mt-4 flex w-full items-center justify-between px-8">
-        <h1 className="text-8xl font-bold text-white">
-          09:41 <span className="text-6xl">AM</span>
-        </h1>
-        <div className="flex items-center space-x-4">
-          <Avatar>
-            <AvatarImage
-              alt="Profile picture"
-              src="/placeholder.svg?height=40&width=40"
-            />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <div>
-            <h2 className="text-lg font-semibold text-white">Circus</h2>
-            <p className="text-sm text-white">TAEYEON</p>
-          </div>
-        </div>
-      </div>
       <div className="flex-1 flex overflow-hidden gap-10 p-6">
         <LeaderboardResizable />
       </div>
